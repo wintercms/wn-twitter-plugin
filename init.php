c2966f173ee63ce713c0570f5f1cac20a64f7ea3
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 14:13:23 -0400

    add class aliases

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+class_alias(Winter\Twitter\Plugin::class, RainLab\Twitter\Plugin::class);
+
+class_alias(Winter\Twitter\Classes\TwitterClient::class, RainLab\Twitter\Classes\TwitterClient::class);
+
+class_alias(Winter\Twitter\Components\FollowButton::class, RainLab\Twitter\Components\FollowButton::class);
+class_alias(Winter\Twitter\Components\Favorites::class, RainLab\Twitter\Components\Favorites::class);
+class_alias(Winter\Twitter\Components\TweetButton::class, RainLab\Twitter\Components\TweetButton::class);
+class_alias(Winter\Twitter\Components\LatestTweet::class, RainLab\Twitter\Components\LatestTweet::class);
+class_alias(Winter\Twitter\Components\EmbedTweet::class, RainLab\Twitter\Components\EmbedTweet::class);
+class_alias(Winter\Twitter\Components\EmbedTimeline::class, RainLab\Twitter\Components\EmbedTimeline::class);
+
+class_alias(Winter\Twitter\Models\Settings::class, RainLab\Twitter\Models\Settings::class);
