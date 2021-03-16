3394ded0368fd89b6f14f1d2ff07ad043be88395
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-16 13:59:49 -0400

    use aliases.php file

---
+++ aliases.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * To allow compatibility with plugins that extend the original RainLab.Twitter plugin, this will alias those classes to
+ * use the new Winter.Twitter classes.
+ */
+$aliases = [
+    // Regular aliases
+    Winter\Twitter\Plugin::class                    => 'RainLab\Twitter\Plugin',
+    Winter\Twitter\Classes\TwitterClient::class     => 'RainLab\Twitter\Classes\TwitterClient',
+    Winter\Twitter\Components\FollowButton::class   => 'RainLab\Twitter\Components\FollowButton',
+    Winter\Twitter\Components\Favorites::class      => 'RainLab\Twitter\Components\Favorites',
+    Winter\Twitter\Components\TweetButton::class    => 'RainLab\Twitter\Components\TweetButton',
+    Winter\Twitter\Components\LatestTweet::class    => 'RainLab\Twitter\Components\LatestTweet',
+    Winter\Twitter\Components\EmbedTweet::class     => 'RainLab\Twitter\Components\EmbedTweet',
+    Winter\Twitter\Components\EmbedTimeline::class  => 'RainLab\Twitter\Components\EmbedTimeline',
+    Winter\Twitter\Models\Settings::class           => 'RainLab\Twitter\Models\Settings',
+];
+
+foreach ($aliases as $original => $alias) {
+    if (!class_exists($alias)) {
+        class_alias($original, $alias);
+    }
+}
--- init.php
+++ init.php
@@ -1,16 +1,3 @@
 <?php
-
-if (!class_exists(RainLab\Twitter\Plugin::class)) {
-    class_alias(Winter\Twitter\Plugin::class, RainLab\Twitter\Plugin::class);
-
-    class_alias(Winter\Twitter\Classes\TwitterClient::class, RainLab\Twitter\Classes\TwitterClient::class);
-
-    class_alias(Winter\Twitter\Components\FollowButton::class, RainLab\Twitter\Components\FollowButton::class);
-    class_alias(Winter\Twitter\Components\Favorites::class, RainLab\Twitter\Components\Favorites::class);
-    class_alias(Winter\Twitter\Components\TweetButton::class, RainLab\Twitter\Components\TweetButton::class);
-    class_alias(Winter\Twitter\Components\LatestTweet::class, RainLab\Twitter\Components\LatestTweet::class);
-    class_alias(Winter\Twitter\Components\EmbedTweet::class, RainLab\Twitter\Components\EmbedTweet::class);
-    class_alias(Winter\Twitter\Components\EmbedTimeline::class, RainLab\Twitter\Components\EmbedTimeline::class);
-
-    class_alias(Winter\Twitter\Models\Settings::class, RainLab\Twitter\Models\Settings::class);
-}
+// Include aliases
+require_once __DIR__ . '/aliases.php';
