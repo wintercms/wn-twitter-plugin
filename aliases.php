797e828f9bbd063b81cf390cb0ac0752de3d1efb
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 12:09:19 -0400

    normalize class names

--- aliases.php
+++ aliases.php
@@ -8,15 +8,15 @@ use Winter\Storm\Support\ClassLoader;
  */
 $aliases = [
     // Regular aliases
-    Winter\Twitter\Plugin::class                    => 'RainLab\Twitter\Plugin',
-    Winter\Twitter\Classes\TwitterClient::class     => 'RainLab\Twitter\Classes\TwitterClient',
-    Winter\Twitter\Components\FollowButton::class   => 'RainLab\Twitter\Components\FollowButton',
-    Winter\Twitter\Components\Favorites::class      => 'RainLab\Twitter\Components\Favorites',
-    Winter\Twitter\Components\TweetButton::class    => 'RainLab\Twitter\Components\TweetButton',
-    Winter\Twitter\Components\LatestTweet::class    => 'RainLab\Twitter\Components\LatestTweet',
-    Winter\Twitter\Components\EmbedTweet::class     => 'RainLab\Twitter\Components\EmbedTweet',
-    Winter\Twitter\Components\EmbedTimeline::class  => 'RainLab\Twitter\Components\EmbedTimeline',
-    Winter\Twitter\Models\Settings::class           => 'RainLab\Twitter\Models\Settings',
+    Winter\Twitter\Plugin::class                    => RainLab\Twitter\Plugin::class,
+    Winter\Twitter\Classes\TwitterClient::class     => RainLab\Twitter\Classes\TwitterClient::class,
+    Winter\Twitter\Components\FollowButton::class   => RainLab\Twitter\Components\FollowButton::class,
+    Winter\Twitter\Components\Favorites::class      => RainLab\Twitter\Components\Favorites::class,
+    Winter\Twitter\Components\TweetButton::class    => RainLab\Twitter\Components\TweetButton::class,
+    Winter\Twitter\Components\LatestTweet::class    => RainLab\Twitter\Components\LatestTweet::class,
+    Winter\Twitter\Components\EmbedTweet::class     => RainLab\Twitter\Components\EmbedTweet::class,
+    Winter\Twitter\Components\EmbedTimeline::class  => RainLab\Twitter\Components\EmbedTimeline::class,
+    Winter\Twitter\Models\Settings::class           => RainLab\Twitter\Models\Settings::class,
 ];
 
 app(ClassLoader::class)->addAliases($aliases);
